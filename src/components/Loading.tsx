75c09d0d81a3187ad3f44ed71a7c3088ba46bce2
Miracle Mark <miraclemark147@gmail.com>
2024-05-24 13:40:55 -0700

    cleaned up unused props

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,12 +1,9 @@
 import { Html } from "@react-three/drei";
-import React from "react";
 
-type Props = {};
-
-export const Loading = (props: Props) => {
+export const Loading = () => {
   return (
     <Html>
-      <div className="absolute z-50 top-0 left-0w-fullh-full flex justify-center items-center">
+      <div className="absolute z-50 top-0 left-0 w-full h-full flex justify-center items-center">
         <div className="w-[10 vw] h-[10vw] rounded-full">Loading...</div>
       </div>
     </Html>
