8d6288618c28b2c548b3aa4df4ece372765a62cc
Miracle Mark <miraclemark147@gmail.com>
2024-05-24 13:00:20 -0700

    cleared unused imports

--- src/components/Iphone.tsx
+++ src/components/Iphone.tsx
@@ -6,12 +6,12 @@ Source: https://sketchfab.com/3d-models/apple-iphone-15-pro-max-black-df17520841
 Title: Apple iPhone 15 Pro Max Black
 */
 
-import React, { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useGLTF, useTexture } from "@react-three/drei";
 
 import * as THREE from "three";
 
-function Model(props) {
+function Model(props: any) {
   const { nodes, materials } = useGLTF("/models/scene.glb");
   const texture = useTexture(props.item.img);
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+//...
+import * as Sentry from "@sentry/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+Sentry.init({
+  dsn: "https://4bf4a2351dd49a5b069aa88502c1c2c6@o4507313056907264.ingest.us.sentry.io/4507313065558016",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
